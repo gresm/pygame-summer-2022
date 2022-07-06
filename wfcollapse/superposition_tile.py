9c7f60d878c7a20c7f386452910552f8de76eb8f
gresm <78505251+gresm@users.noreply.github.com>
2022-07-06 13:43:11 +0200

    Superposition tiles.

--- wfcollapse/superposition_tile.py
+++ wfcollapse/superposition_tile.py
@@ -1,7 +1,17 @@
 """
 Script for tiles in superpositions.
 """
+from __future__ import annotations
 
 
 class SuperpositionTile:
-    pass
+    def __init__(self, superpositions: set[int]):
+        self.superpositions = superpositions.copy()
+
+    @property
+    def collapsed(self):
+        return len(self.superpositions) == 1
+
+    @property
+    def unsolvable(self):
+        return len(self.superpositions) == 0

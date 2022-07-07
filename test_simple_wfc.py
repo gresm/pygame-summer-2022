9a5bd95daaf7ef5e7b1ac3ab2565ddb69087cb83
gresm <78505251+gresm@users.noreply.github.com>
2022-07-07 12:04:21 +0200

    Better debugging?

--- test_simple_wfc.py
+++ test_simple_wfc.py
@@ -7,23 +7,36 @@ from wfcollapse.board import Board2d, BoardTile
 from wfcollapse.superposition_tile import SuperpositionTile
 from wfcollapse.simple_wfc import SimpleCollapse
 
-
 if __name__ != '__main__':
     raise RuntimeError('This file is not meant to be imported')
 
 
-def draw_board(board: list[list[BoardTile[SuperpositionTile]]]):
-    for line in board:
+def in_color(text: str, foreground, background):
+    mat = f'\33[38;2;{";".join(map(str, foreground))};48;2;{";".join(map(str, background))}m'
+    return f"{mat}{text}\33[0m"
+
+
+def draw_board(board: list[list[BoardTile[SuperpositionTile]]], mark: tuple[int, int] | None = None):
+    for x in range(len(board)):
+        line = board[x]
         l1 = ""
         l2 = ""
-        for el in line:
+        for y in range(len(line)):
+            el = line[y]
             lst = list(el.tile.superpositions)
             lst.sort()
 
-            l1 += str(lst[0]) if len(lst) > 0 else '.'
-            l1 += str(lst[1]) if len(lst) > 1 else "."
-            l2 += str(lst[2]) if len(lst) > 2 else "."
-            l2 += str(lst[3]) if len(lst) > 3 else "."
+            el_l1 = str(lst[0]) if len(lst) > 0 else '.'
+            el_l1 += str(lst[1]) if len(lst) > 1 else "."
+            el_l2 = str(lst[2]) if len(lst) > 2 else "."
+            el_l2 += str(lst[3]) if len(lst) > 3 else "."
+
+            if mark is not None and x == mark[0] and y == mark[1]:
+                el_l1 = in_color(el_l1, (0, 0, 0), (255, 255, 255))
+                el_l2 = in_color(el_l2, (0, 0, 0), (255, 255, 255))
+
+            l1 += el_l1
+            l2 += el_l2
 
             l1 += " "
             l2 += " "
@@ -42,18 +55,17 @@ def frame():
 
 def test_simple_wfc():
     collapse = SimpleCollapse(Board2d(10, 10, SuperpositionTile({0, 1, 2, 3})),
-                              {0: set(), 1: set(), 2: set(), 3: set()})
+                              {0: {1}, 1: {2}, 2: set(), 3: set()})
 
     frame()
     draw_board(collapse.board.board)
     inc = 0
 
     for step in collapse.solve():
-        for _ in step:
-            pass
-        frame()
-        draw_board(collapse.board.board)
-        print("step: ", inc)
+        for pos in step:
+            frame()
+            draw_board(collapse.board.board, pos)
+            print("step: ", inc)
         inc += 1
 
     frame()

e7303442dbf35e8e92444089bf29fe7e4f40e407
gresm <78505251+gresm@users.noreply.github.com>
2022-07-09 15:27:16 +0200

    Add play text.

--- game/scenes.py
+++ game/scenes.py
@@ -9,11 +9,15 @@ class MainMenu(BaseScene):
     menu_text_rect: pg.Rect
     screen_rect: pg.Rect
     widgets_rect: pg.Rect
+    play_rect: pg.Rect
+    play_text: pg.Surface
 
     def init(self):
         pg.display.set_caption("The Climb - Main menu", "The climb")
         self.menu_text = assets.title_font.render("Main Menu", True, (255, 255, 255))
+        self.play_text = assets.title_font.render("Play", True, (255, 255, 255))
         self.menu_text_rect = self.menu_text.get_rect()
+        self.play_rect = self.play_text.get_rect()
 
     def after_init(self):
         self.screen_rect = self.manager.game.screen.get_rect()
@@ -21,10 +25,13 @@ class MainMenu(BaseScene):
         self.widgets_rect.center = pg.Vector2(self.screen_rect.midbottom) - pg.Vector2(0, 100)
         self.menu_text_rect.midbottom = pg.Vector2(self.screen_rect.midbottom) - pg.Vector2(0, 20)
 
+        self.play_rect.midleft = pg.Vector2(self.widgets_rect.midleft) + pg.Vector2(20, 0)
+
     def draw(self, window: pg.Surface):
         window.fill((0, 0, 0))
         pg.draw.rect(window, (255, 0, 255), self.widgets_rect)
         window.blit(self.menu_text, self.menu_text_rect)
+        window.blit(self.play_text, self.play_rect)
 
 
 __all__ = ['MainMenu']

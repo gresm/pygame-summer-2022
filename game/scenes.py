6fd3d3707753b3bf83649cea7c3108ce78b9b3fa
gresm <78505251+gresm@users.noreply.github.com>
2022-07-09 15:55:02 +0200

    Basic selection.

--- game/scenes.py
+++ game/scenes.py
@@ -4,6 +4,11 @@ from gamelib import BaseScene
 from . import assets
 
 
+class QuitScene(BaseScene):
+    def after_init(self):
+        self.manager.game.stop()
+
+
 class MainMenu(BaseScene):
     menu_text: pg.Surface
     menu_text_rect: pg.Rect
@@ -19,6 +24,8 @@ class MainMenu(BaseScene):
     quit_rect: pg.Rect
     quit_text: pg.Surface
 
+    currently_selecting: int
+
     def init(self):
         pg.display.set_caption("The Climb - Main menu", "The climb")
         self.menu_text = assets.title_font.render("Main Menu", True, (255, 255, 255))
@@ -31,6 +38,8 @@ class MainMenu(BaseScene):
         self.settings_rect = self.settings_text.get_rect()
         self.quit_rect = self.quit_text.get_rect()
 
+        self.currently_selecting = 0
+
     def after_init(self):
         self.screen_rect = self.manager.game.screen.get_rect()
         self.widgets_rect = pg.Rect(0, 0, self.screen_rect.width - 200, 100)
@@ -41,6 +50,10 @@ class MainMenu(BaseScene):
         self.settings_rect.center = self.widgets_rect.center
         self.quit_rect.midright = pg.Vector2(self.widgets_rect.midright) - pg.Vector2(50, 0)
 
+    @staticmethod
+    def _draw_line_under_rect(surface: pg.Surface, color: tuple[int, int, int], rect: pg.Rect):
+        pg.draw.line(surface, color, rect.bottomleft, rect.bottomright)
+
     def draw(self, window: pg.Surface):
         window.fill((0, 0, 0))
         pg.draw.rect(window, (255, 0, 255), self.widgets_rect)
@@ -49,6 +62,15 @@ class MainMenu(BaseScene):
         window.blit(self.play_text, self.play_rect)
         window.blit(self.settings_text, self.settings_rect)
         window.blit(self.quit_text, self.quit_rect)
+
+        if self.currently_selecting == 1:
+            self._draw_line_under_rect(window, (255, 255, 255), self.play_rect)
+        elif self.currently_selecting == 2:
+            self._draw_line_under_rect(window, (255, 255, 255), self.settings_rect)
+        elif self.currently_selecting == 3:
+            self._draw_line_under_rect(window, (255, 255, 255), self.quit_rect)
+        elif self.currently_selecting == 4:
+            self._draw_line_under_rect(window, (255, 255, 255), self.menu_text_rect)
 
 
 __all__ = ['MainMenu']
